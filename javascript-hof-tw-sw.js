e10dfa5c46553b07e8f3eb3be10bd12383b240ba
LEARN Academy <learnacademy@LEARNs-Air-2.attlocal.net>
2022-06-21 16:43:17 -0700

    HOF challenges-tw-sw

---
+++ javascript-hof-tw-sw.js
@@ -0,0 +1,60 @@
+// 1. Write a function that takes in an array of numbers and returns an array with all numbers multiplied by 3.
+
+// var testArr1 = [3, 9, 15, 4, 10]
+
+// var myArr1 = [3, 9, 15, 4, 10]
+
+// const mult3 = (array) => {
+//   let newArr = []
+//   for(let i=0; i<array.length; i++){
+//     newArr.push(array[i] * 3)
+//   }
+//   return newArr
+// }
+
+// console.log(mult3(myArr1))
+
+
+// 2. Write a function that takes in an array of numbers and returns a new array with only odd numbers.
+
+// var myArr2 = [0, 2, -7, 3, 5, 8, 10, 13]
+
+// const onlyOdd = (array) => {
+//   let newArr = []
+//   for(let i=0; i<array.length; i++){
+//     if(array[i] % 2 !== 0){
+//         console.log(`odd`)
+//        newArr.push(array[i])
+//     }
+//   }
+//   return newArr
+// }
+
+// console.log(onlyOdd(myArr2))
+
+
+// 3. Write a function that takes in an array of numbers and letters and returns a string with only the letters. HINT: use the typeof method.
+
+var comboArr = [7, "n", "i", "c", 10, "e", false, "w", 3, "o", "r", "k"]
+
+const myFilteredArray = comboArr.filter(value => {
+    return string.filter
+  })
+  
+  console.log(myFilteredArray)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
